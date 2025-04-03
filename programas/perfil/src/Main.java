79b274fb43e136c3874d77e8c0a11ac60ca37238
Fabricio Augusto Oliveira Bastos <168025112+Gutobastos@users.noreply.github.com>
2025-04-03 13:45:44 -0300

    Update Main.java

--- programas/perfil/src/Main.java
+++ programas/perfil/src/Main.java
@@ -1,10 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Olá, Fabricio Bastos!");
-        System.out.println("Tudo bem?");
+        System.out.println("Esse é o Screen Match!");
+        //System.out.println("Top Gun: Maverik");
+
+        int anoDeLancamento = 2022;
+        //System.out.println("Ano de Lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        //System.out.println("Nota do Filme: " + media);
+        String sinopse;
+        sinopse = """
+                Top Gun: Maverik
+                Filme de ação com galã dos anos 80.
+                Muito bom
+                Ano de Lançamento: """+ anoDeLancamento;
+        System.out.println(sinopse);
+        System.out.println("Nota do Filme: " + media);
     }
 }
