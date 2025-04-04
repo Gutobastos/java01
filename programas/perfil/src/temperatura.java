0c782e6270f9c15c6054c60ef8133bef0b1f92bc
Fabricio Augusto Oliveira Bastos <168025112+Gutobastos@users.noreply.github.com>
2025-04-04 13:25:18 -0300

    Delete programas/perfil/src/temperatura.java

--- programas/perfil/src/temperatura.java
+++
@@ -1,6 +0,0 @@
-public class temperatura {
-    public static void main(String[] args){
-        int temperatura = 30;
-        System.out.println("Fahrenheit :" + (int)((temperatura * 1.8) + 32) + " F°");
-    }
-}
